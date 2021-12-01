e0741e07899d39314be47059029d6fd5cebc2b90
Chen Tzuwei <chen.tzuwei@gmail.com>
2021-12-01 23:07:31 +0800

    initial

---
+++ app/src/main/java/razgriz/self/appexercise1202/MainActivity.java
@@ -0,0 +1,14 @@
+package razgriz.self.appexercise1202;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
